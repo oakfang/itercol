76a6ab8be4fcfcb6ca1eeb6b8f43c8e634db6372
Alon Niv <oakfang@gmail.com>
2016-03-27 10:03:23 +0300

    Actual code

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # itercol
 A lazy, generator-based, JS library for handling sequences in a concise, efficient way.
+
+## API
+### class:Iterator(iterator)
+Create a new instance of an Iterator by passing its constructor an iterator.
+
+```js
+const iter = new Iterator(new Set([1, 2, 3, 4]));
+```
+
+### Iterator::invoke(methodName, ...args)
+Invoke a method of the wrapped iterator.
+
+```js
+const iter = new Iterator(new Set([1, 2, 3, 4]));
+iter.invoke('add', 5);
+```
+
+### Iterator::filter(predicate)
+Return a filtered iterator (lazy).
+
+### Iterator::map(mapper)
+Return a mapped iterator (lazy).
+
+### Iterator::take(limit)
+Return a limited iterator.
+
+### Iterator::reduce(reducer, accumolator)
+Reduce an iterator in O(n), instead of consuming it (O(n)) and then reducing the resulting array (O(n)).
+
+## Usage
+```js
+'use strict';
+
+const Iterator = require('synograph/gentools');
+const iter = new Iterator(new Set([1, 2, 3, 4]));
+for (let item of iter.filter(x => console.log(x) || x > 2).map(x => x * 2)) {
+    console.log(item);
+}
+/* this will print :
+1
+2
+3
+6
+4
+8
+*/
+
+let arr = Array.from(iter); // easily consume the iterator :)
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+class Iterator {
+    constructor(inner) {
+        this._ = inner;
+    }
+
+    [Symbol.iterator]() {
+        return this._[Symbol.iterator]();
+    }
+
+    invoke() {
+        const args = Array.from(arguments);
+        return this._[args[0]].apply(this._, args.slice(1));
+    }
+
+    filter(predicate) {
+        let self = this;
+        return new Iterator((function*() {
+            for (let item of self._) {
+                if (predicate(item)) yield item;
+            }
+        })());
+    }
+
+    map(mapper) {
+        let self = this;
+        return new Iterator((function*() {
+            for (let item of self._) {
+                yield mapper(item);
+            }
+        })());
+    }
+
+    reduce(reducer, accumolator) {
+        for (let item of this._) {
+            accumolator = reducer(accumolator, item);
+        }
+        return accumolator;
+    }
+
+    take(limit) {
+        let self = this;
+        return new Iterator(function* () {
+            for (let item of self._) {
+                if (!limit--) return;
+                yield item;
+            }
+        }());
+    }
+}
+
+module.exports = Iterator;
